5b5ed0de7b8c0aa1e9e909d4fc36a532df5a9850
Helena Magaldi <helenamagaldi@gmail.com>
2020-11-14 09:25:02 -0300

    adding new files

---
+++ Projeto_1/lista.py
@@ -0,0 +1,11 @@
+lista_vazia = []
+
+lista_numeros = [1, 2, 4, 6]
+
+lista_strings = ['helena', 'raíra']
+
+lista_mista = [1, 'helena', lista_numeros]
+
+print(lista_mista)
+
+print(lista_numeros[1])
---
+++ Projeto_1/test1.py
@@ -0,0 +1,11 @@
+a_string = "nome"
+capitalized_string = a_string.capitalize()
+print(capitalized_string)
+
+# with input
+nome = input("digite o nome")
+print(f"{nome.capitalize()}")
+
+nome2 = "helena"
+nome2.capitalize()
+print(nome2)
